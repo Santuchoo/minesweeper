86c26805978f8ae80e720e041e335b6751514f0b
Santino Vitanzi <santinovitanzi@gmail.com>
2026-05-25 18:53:24 -0300

    Added grid dynamic creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { Square } from "./components/Square"
+
 function App() {
+  function getGrid(gridSizeX, gridSizeY) {
+    const grid = []
+    for (let row = 0; row < gridSizeY; row++) {
+      const rowArr = []
+      for (let i=0; i < gridSizeX; i++) {
+        rowArr.push(<Square
+          key={[row, i]} //row, col
+        ></Square>)
+      }
+      grid.push(rowArr)
+    }
+    
+    return grid
+  }
+
   return (
     <>
-      
+      <header>
+        <h1 className="text-white text-4xl font-bold">Minesweeper</h1>
+      </header>
+      <main className="flex flex-col gap-1">
+        {getGrid(8, 8).map((row)=> {
+          return (
+            <div className="flex gap-1">
+              {row.map((el)=> {
+                return el
+              })}
+            </div>
+          )
+        })}
+      </main>
     </>
   )
 }
---
+++ src/components/Square.jsx
@@ -0,0 +1,7 @@
+export function Square(/* { explosive, position } */) {
+  return (
+    <>
+      <div className="size-12 bg-gray-500"></div>
+    </>
+  )
+}
